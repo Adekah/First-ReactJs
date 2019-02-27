752e0ee7fea64f30767fd7d3c6f91d42e8f35a37
Adekah <36799323+Adekah@users.noreply.github.com>
2019-02-28 02:11:28 +0300

    reactjs-myportfolio-P2
    reactjs-myportfolio-P2

--- reactjs-myportfolio/src/components/landingpage.js
+++ reactjs-myportfolio/src/components/landingpage.js
@@ -1,13 +1,40 @@
 import React, {Component} from 'react';
+import {Grid, Cell} from "react-mdl";
 
 
 class Landing extends Component {
     render() {
         return (
-            <div>
-                <h1>Landing Page</h1>
+            <div style={{width: '100%', margin: 'auto'}}>
+                <Grid className="landing-grid">
+                    <Cell col={12}>
+                        <img className="avatar-image"
+                             src="https://cdn.iconscout.com/icon/free/png-256/avatar-375-456327.png"/>
+                        <div className="banner-text">
+                            <h1>Full Stack Web Developer</h1>
+                            <hr/>
+                            <p>ASP.NET MVC | Microsoft SQL Server | C# | JavaScript | React | HTML/CSS | Bootstrap</p>
+                            <div className="social-links">
+                                {/*Linkedin*/}
+                                <a href="https://www.google.com" target="_blank">
+                                    <i className="fa fa-linkedin-square" aria-hidden="true"></i>
+                                </a>
+                                {/* */}
+                                <a href="https://www.google.com">
+                                    <i className="fa fa-twitter-square" aria-hidden="true"></i>
+                                </a>
+                                {/* */}
+                                <a href="https://www.google.com">
+                                    <i className="fa fa-github-square" aria-hidden="true"></i>
+                                </a>
+                            </div>
+                        </div>
+                    </Cell>
+                </Grid>
+
             </div>
         );
     }
 }
+
 export default Landing;
