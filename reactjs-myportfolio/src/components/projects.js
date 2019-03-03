95695ead1bc0823594acb189a276e7cb21eab538
Adekah <36799323+Adekah@users.noreply.github.com>
2019-03-03 03:10:16 +0300

    reactjs-myportfolio-P3

--- reactjs-myportfolio/src/components/projects.js
+++ reactjs-myportfolio/src/components/projects.js
@@ -1,13 +1,107 @@
 import React, {Component} from 'react';
-
+import {Tabs, Tab, Grid, Cell, Card, CardMenu, CardText, CardTitle, CardActions, Button, IconButton} from "react-mdl";
 
 class Projects extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {activeTab: 0};
+    }
+
+    toggleCategories() {
+        if (this.state.activeTab === 0) {
+            return (
+                <div className="projects-grid">>
+                <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                    <CardTitle style={{
+                        color: 'black',
+                        height: '176px',
+                        background: 'url(https://goo.gl/L7o29g)center / cover'  }}>
+                   .NET MVC PROJECT #1
+                    </CardTitle>
+                        <CardText>
+                            Lorem Ipsum, dizgi ve baskı endüstrisinde kullanılan mıgır metinlerdir.
+                        </CardText>
+                        <CardActions border>
+                            <Button>Github</Button>
+                            <Button>LiveDemo</Button>
+                        </CardActions>
+                        <CardMenu style={{color: '#fff'}}>
+                            <IconButton name="share"/>
+                        </CardMenu>
+
+                </Card>
+                    <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                        <CardTitle style={{
+                            color: 'black',
+                            height: '176px',
+                            background: 'url(https://goo.gl/L7o29g)center / cover'  }}>
+                            .NET MVC PROJECT #1
+                        </CardTitle>
+                        <CardText>
+                            Lorem Ipsum, dizgi ve baskı endüstrisinde kullanılan mıgır metinlerdir.
+                        </CardText>
+                        <CardActions border>
+                            <Button>Github</Button>
+                            <Button>LiveDemo</Button>
+                        </CardActions>
+                        <CardMenu style={{color: '#fff'}}>
+                            <IconButton name="share"/>
+                        </CardMenu>
+
+                    </Card>
+                    <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                        <CardTitle style={{
+                            color: 'black',
+                            height: '176px',
+                            background: 'url(https://goo.gl/L7o29g)center / cover'  }}>
+                            .NET MVC PROJECT #1
+                        </CardTitle>
+                        <CardText>
+                            Lorem Ipsum, dizgi ve baskı endüstrisinde kullanılan mıgır metinlerdir.
+                        </CardText>
+                        <CardActions border>
+                            <Button>Github</Button>
+                            <Button>LiveDemo</Button>
+                        </CardActions>
+                        <CardMenu style={{color: '#fff'}}>
+                            <IconButton name="share"/>
+                        </CardMenu>
+
+                    </Card>
+                </div>
+            )
+        } else if (this.state.activeTab === 1) {
+            return (
+                <div><h2>This is React</h2></div>
+            )
+        } else if (this.state.activeTab === 2) {
+            return (
+                <div><h2>This is JavaScript</h2></div>
+            )
+        }
+    }
+
+
     render() {
         return (
-            <div>
-                <h1>Projects Page</h1>
+            <div className="category-tabs">
+                <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({activeTab: tabId})} ripple>
+                    <Tab>ASP.NET MVC</Tab>
+                    <Tab>React</Tab>
+                    <Tab>JavaScript</Tab>
+                </Tabs>
+
+                    <Grid >
+                        <Cell col={12}>
+                            <div className="contennt">   {this.toggleCategories()} </div>
+                        </Cell>
+                    </Grid>
+
+
+
             </div>
         );
     }
 }
+
 export default Projects;
